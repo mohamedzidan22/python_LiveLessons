b56d6d00ce1ee22b9501abf4354c1de6564de221
MOHAMED <mohamedzidanzz56@gmail.com>
2024-03-18 17:14:21 +0200

    added the sentinel_loop snippet. used when i don't know how many time i should iterate

---
+++ sentinel_loop.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 18 16:52:11 2024
+
+@author: MOHAMED
+"""
+
+""" Sentinel iteration used when i don't know how many times i want to iterate"""
+
+#intialization phase
+total = 0
+grade_counter = 0
+
+#process phase
+grade = int(input('enter your grade, -2 to terminate: '))
+while grade != -2:
+    total = grade + total
+    grade_counter += 1
+    grade = int(input('enter your grade, -2 to terminate: '))
+
+#termination phase
+if grade == -2:
+    print(f'the avg grade is = {total / grade_counter}')
